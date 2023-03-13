9144d260b9690cd95d0b3214db26147396cf76a7
Marat Gumerov <gm.gameoff@gmail.com>
2023-03-13 18:15:56 +0500

    create project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * mega16_test_uart_no_irq.cpp
+ *
+ * Created: 13.03.2023 18:14:25
+ * Author : User
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
